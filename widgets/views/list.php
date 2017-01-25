e06b31c471d24554564f9a7329d39c6b323ae6ef
Ryabov Valentin <valentin531@yandex.ru>
2017-01-26 00:31:28 +0500

    hardcoded add comment tmplat

--- widgets/views/list.php
+++ widgets/views/list.php
@@ -36,24 +36,34 @@ CommentsAsset::register($this);
                         </div>
                     </div>
                 </div>
-                <?php if (!empty($comment->childs)) { ?>
                 <div class="qv-comment-replies">
-                    <?php foreach ($comment->childs as $reply) { ?>
-                        <div class="qv-comment-reply">
-                            <div class="qv-reply-rating"><?= rand(0, 100); ?></div>
-                            <div class="qv-reply-rate">
-                                <a class="qv-reply-rate-btn qv-rate-down" href="javascript:void(0)">-</a>
-                                <a class="qv-reply-rate-btn qv-rate-up" href="javascript:void(0)">+</a>
+                    <?php if (!empty($comment->childs)) { ?>
+                        <?php foreach ($comment->childs as $reply) { ?>
+                            <div class="qv-comment-reply">
+                                <div class="qv-reply-rating"><?= rand(0, 100); ?></div>
+                                <div class="qv-reply-rate">
+                                    <a class="qv-reply-rate-btn qv-rate-down" href="javascript:void(0)">-</a>
+                                    <a class="qv-reply-rate-btn qv-rate-up" href="javascript:void(0)">+</a>
+                                </div>
+                                <div class="qv-reply-text">
+                                    <?= $reply->text ?><span
+                                            class="qv-reply-info"> – <?= call_user_func($userShowCallback, $reply->user); ?>
+                                        [<?= rand(0, 100); ?>], <?= date('d.m.Y H:i', $reply->created_at); ?></span>
+                                </div>
                             </div>
-                            <div class="qv-reply-text">
-                                <?= $reply->text ?><span class="qv-reply-info"> – <?= call_user_func($userShowCallback, $reply->user); ?>
-                                    [<?= rand(0, 100); ?>], <?= date('d.m.Y H:i', $reply->created_at); ?></span>
-                            </div>
-                        </div>
+                        <?php } ?>
                     <?php } ?>
+                    <p class="qv-comment-link-block"><a href="#" class="qv-comment-link">ответить</a></p>
                 </div>
-                <?php } ?>
             </div>
         </div>
     <?php } ?>
 </div>
+
+<form action="" class="qv-comment-add-form">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Написать комментарий</label>
+        <textarea name="aaa" id="exampleInputEmail1" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+</form>
+
